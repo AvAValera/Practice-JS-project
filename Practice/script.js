3dc9faa5fd875b951819fa2b896cf0074be66049
AvAValera <avavarenik07@gmail.com>
2021-03-17 10:45:10 +0200

    add learn project

--- Practice/script.js
+++ Practice/script.js
@@ -1,7 +1,18 @@
+const wrapper = document.querySelector('.wrapper');
 const cube = document.querySelectorAll('.cube');
 
 cube.forEach((el, i) => {
-    el.addEventListener('click', () => {
-        cube[i].classList.add('hidden')
+    console.log(i);
+    
+    el.addEventListener('click', (e) => {
+        e.target.classList.add('hidden');
+        setTimeout(() =>{
+            e.target.classList.remove('hidden');
+        },2000)
     })
 })
+const circle = document.createElement('div');
+circle.classList.add('circle')
+
+cube[1].prepend(circle);
+cube[2].insertAdjacentHTML("beforebegin", '<h1>Hello</h1>');
---
+++ Task/script.js
@@ -0,0 +1,24 @@
+'use strict';
+// const numberOfFilms = {
+//     count: '',
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+// const count = prompt('Сколько фильмов вы уже посмотрели?')
+// const movie = prompt('Один из последних просмотренных фильмов?');
+// const rating = +prompt('на сколько оцените его?');
+
+
+// numberOfFilms.movies[movie] = rating;
+// numberOfFilms.count = count;
+// console.log(numberOfFilms);
+
+const dataBase = {
+    count: 0,
+    fun: function(){
+        this.count++;
+    }
+}
+
