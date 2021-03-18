4278ae50f18626a596404f358a59c10213f04645
AvAValera <avavarenik07@gmail.com>
2021-03-18 15:26:56 +0200

    add tabs in Food project

---
+++ Food/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const tabMenu = document.querySelector('.tabheader__items'),
+        tabMenuItem = tabMenu.querySelectorAll('.tabheader__item'),
+        tabContainer = document.querySelectorAll('.tabcontent');
+
+    function hideTab(){
+        tabContainer.forEach(el => {
+            el.style.display = 'none';
+        })
+        tabMenuItem.forEach(el => {
+            el.classList.remove('tabheader__item_active')
+        })
+    }
+    function showTab(i = 0){
+        tabContainer[i].style.display = 'block';
+        tabMenuItem[i].classList.add('tabheader__item_active')
+    }
+
+    hideTab();
+    showTab();
+    tabMenu.addEventListener('click', (e) => {
+        if(e.target.classList.contains('tabheader__item')){
+            tabMenuItem.forEach((el, i) => {
+                if(e.target == el){
+                    hideTab();
+                    showTab(i);
+                }
+            })
+        }
+    })
+})
+
+
