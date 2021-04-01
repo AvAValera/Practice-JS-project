16b7e7f4f4bdd578b71c1cfb4426066997fb4d63
AvAValera <avavarenik07@gmail.com>
2021-04-01 15:38:45 +0300

    edit to do project

--- To Do/script.js
+++ To Do/script.js
@@ -1,20 +1,58 @@
+const listBlock = document.querySelector('.container__list')
 const list = [
-    'By milk',
-    'Go work',
-    'Walking with dog',
-    'Go to sleep'
+    {name : 'By milk', ready : false},
+    {name : 'Go to work', ready : true},
+    {name : 'Walking with dog', ready : false},
+    {name : 'Go to sleep', ready : true},
+    {name : 'Kiss douther', ready : false},
 ]
-
-function addItenList(array) {
-    const listBlock = document.querySelector('.container__list')
-    array.map((el) => {
-        const checkbox = document.createElement('input')
-        checkbox.setAttribute('type', 'checkbox')
-        const li = document.createElement('li')
-        li.classList.add('item')
-        li.textContent = el
-        li.append(checkbox)
-        listBlock.append(li)
+function sort (){
+    const item = document.querySelectorAll('li')
+    item.forEach(el => {
+        if(el.classList.contains('checked')){
+            listBlock.after(el)
+        }
     })
 }
+
+function addItenList(array) {
+    array.map((el) => {
+        const checkbox = document.createElement('input')
+        const li = document.createElement('li')
+        checkbox.setAttribute('type', 'checkbox')
+        li.classList.add('item')
+        checkbox.classList.add('check-item')
+        li.textContent = el.name
+        checkbox.checked = el.ready
+        if(el.ready) li.classList.add('checked')
+        li.prepend(checkbox)
+        listBlock.append(li)
+    })
+    sort()
+}
+
 addItenList(list)
+
+function addList(){
+    const input = document.querySelector('.text-input')
+    const btn = document.querySelector('.btn-input')
+    btn.addEventListener('click', () => {
+        if(input.value){
+            const checkbox = document.createElement('input')
+            const li = document.createElement('li')
+            checkbox.setAttribute('type', 'checkbox')
+            li.classList.add('item')
+            checkbox.classList.add('check-item')
+            li.textContent = input.value
+            li.prepend(checkbox)
+            listBlock.append(li)
+            input.value = '';
+        }
+        else{
+            
+            
+        }
+    })
+}
+addList()
+
