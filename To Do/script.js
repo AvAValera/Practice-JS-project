30c02b93696bb31c7c48e2309c9f2a531e951ad5
AvAValera <avavarenik07@gmail.com>
2021-04-02 10:54:59 +0300

    edit to do project

--- To Do/script.js
+++ To Do/script.js
@@ -1,21 +1,18 @@
 const listBlock = document.querySelector('.container__list')
 const list = [
-    {name : 'By milk', ready : false},
+    {name : 'Buy milk', ready : false},
     {name : 'Go to work', ready : true},
     {name : 'Walking with dog', ready : false},
     {name : 'Go to sleep', ready : true},
-    {name : 'Kiss douther', ready : false},
+    {name : 'Kiss daughter', ready : false},
 ]
 function sort (){
-    const item = document.querySelectorAll('li')
-    item.forEach(el => {
-        if(el.classList.contains('checked')){
-            listBlock.after(el)
-        }
-    })
+    list.sort((a, b) => a.ready - b.ready)
 }
 
 function addItenList(array) {
+    listBlock.innerHTML = ' '
+    sort();
     array.map((el) => {
         const checkbox = document.createElement('input')
         const li = document.createElement('li')
@@ -28,7 +25,6 @@ function addItenList(array) {
         li.prepend(checkbox)
         listBlock.append(li)
     })
-    sort()
 }
 
 addItenList(list)
@@ -36,23 +32,37 @@ addItenList(list)
 function addList(){
     const input = document.querySelector('.text-input')
     const btn = document.querySelector('.btn-input')
+    const message = document.querySelector('.message')
     btn.addEventListener('click', () => {
         if(input.value){
-            const checkbox = document.createElement('input')
-            const li = document.createElement('li')
-            checkbox.setAttribute('type', 'checkbox')
-            li.classList.add('item')
-            checkbox.classList.add('check-item')
-            li.textContent = input.value
-            li.prepend(checkbox)
-            listBlock.append(li)
+            message.textContent = ''
+            list.push({name : input.value, ready : false})
             input.value = '';
+            addItenList(list)
         }
         else{
-            
+            message.textContent = 'Must be not empty'
             
         }
     })
 }
 addList()
 
+function checkElement(){
+    const check = document.querySelectorAll('input.check-item')
+    check.forEach((el, i) =>{
+        el.addEventListener('click', (e) =>{
+            if(e.target.checked){
+                el.parentElement.classList.add('checked')
+                list[i].ready = true
+            }
+            else{
+                el.parentElement.classList.remove('checked')
+                list[i].ready = false
+            }
+            console.log(list);
+            
+        })
+    })
+}
+checkElement()
